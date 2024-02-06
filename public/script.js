e47cd830db3b6f6804c799409fa8c9fffb9f2a10
exersonz <cinty.lin.cinty@gmail.com>
2024-02-05 18:07:36 -0800

    working video chat app

---
+++ public/script.js
@@ -0,0 +1,154 @@
+const socket = io("/");
+
+var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "443",
+});
+
+const user = prompt("Enter your name");
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+let myStream;
+
+navigator.mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: true,
+    })
+    .then((stream) => {
+        myStream = stream;
+        addVideoStream(myVideo, stream);
+
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+        });
+
+        // you are already in the video call and seeing others joining one by one
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+
+            // checks/monitors stream of the new person who joins the call after I have joined
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        })
+    })
+
+// function to append another user's video into the video grid
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
+        $("#video_grid").append(video);
+    });
+};
+
+function connectToNewUser(userId, stream){
+    // calling peer to receive streams or answer their calls
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+}
+
+$(function () {
+    $("#show_chat").click(function () {
+        $(".left-window").css("display", "none")
+        $(".right-window").css("display", "block")
+        $(".header_back").css("display", "block")
+    })
+    $(".header_back").click(function () {
+        $(".left-window").css("display", "block")
+        $(".right-window").css("display", "none")
+        $(".header_back").css("display", "none")
+    })
+
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+    $("#stop_video").click(function(){
+        // fetching wheter or not the system has enabled video and toggling it when pressing mute btn
+        const enabled = myStream.getVideoTracks()[0].enabled;
+        if(enabled){
+            myStream.getVideoTracks()[0].enabled = false;
+            html = `<i class = "fa fa-video-slash"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html); // applying changes
+        }
+        else{
+            myStream.getVideoTracks()[0].enabled = true;
+            html = `<i class = "fa fa-video"></i>`;
+            $("#stop_video").toggleClass("background_blue");
+            $("#stop_video").html(html); // applying changes
+        }
+    })
+
+    $("#mute_button").click(function(){
+        // fetching wheter or not the system has enabled audio and toggling it when pressing mute btn
+        const enabled = myStream.getAudioTracks()[0].enabled;
+        if(enabled){
+            myStream.getAudioTracks()[0].enabled = false;
+            html = `<i class = "fa fa-microphone-slash"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html); // applying changes
+        }
+        else{
+            myStream.getAudioTracks()[0].enabled = true;
+            html = `<i class = "fa fa-microphone"></i>`;
+            $("#mute_button").toggleClass("background_blue");
+            $("#mute_button").html(html); // applying changes
+        }
+    })
+
+    $("#invite_button").click(function(){
+        const to = prompt("Enter the recipient's email address for sharing: ")
+        let data = {
+            url: window.location.href,
+            to: to
+        }
+
+        $.ajax({
+            url: '/send-mail',
+            type: 'post',
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function(result){
+                alert("Invitation sent!")
+            },
+            error: function(result){
+                console.log(result.responseJSON)
+            }
+        })
+    })
+})
+
+peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id, user);
+});
+
+socket.on("createMessage", (message, userName) => {
+    $(".messages").append(`
+        <div class="message">
+            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
+        }</span> </b>
+            <span>${message}</span>
+        </div>
+    `)
+});
